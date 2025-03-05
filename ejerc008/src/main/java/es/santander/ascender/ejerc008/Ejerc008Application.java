af4a7d625cea55bfff51d7d15f45547fd812b24a
Iryna Bohomiakova <irinkavakula@gmail.com>
2025-03-05 13:13:13 +0100

    Add readme

---
+++ ejerc008/src/main/java/es/santander/ascender/ejerc008/Ejerc008Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejerc008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerc008Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerc008Application.class, args);
+	}
+
+}
---
+++ ejerc008/src/test/java/es/santander/ascender/ejerc008/Ejerc008ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejerc008;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerc008ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
